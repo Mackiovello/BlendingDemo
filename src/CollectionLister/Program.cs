eea79c11d753720514cec5b266e011f95bca6c8e
Erlend <elandroe@icloud.com>
2017-05-10 10:18:04 +0200

    add stuff to the database

--- src/CollectionLister/Program.cs
+++ src/CollectionLister/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Starcounter;
 using CollectionLister.Handlers;
+using SharedModel;
 
 namespace CollectionLister
 {
@@ -8,6 +9,33 @@ namespace CollectionLister
     {
         static void Main()
         {
+            bool listIsEmpty = Db.SQL("SELECT p FROM SharedModel.PetOwner p").First == null;
+
+            if (listIsEmpty)
+            {
+                Db.Transact(() =>
+                {
+                    new PetOwner
+                    {
+                        Name = "Emma",
+                        Pet = new Pet { Name = "Lassie", Type = PetType.Dog },
+                    };
+
+                    new PetOwner
+                    {
+                        Name = "James",
+                        Pet = new Pet { Name = "Tiger", Type = PetType.Cat }
+                    };
+
+
+                    new PetOwner
+                    {
+                        Name = "Mary",
+                        Pet = new Pet { Name = "Nemo", Type = PetType.Fish }
+                    };
+                });
+            }
+
             var main = new MainHandlers();
             var partial = new PartialHandlers();
 
--- src/SharedModel/Pet.cs
+++ src/SharedModel/Pet.cs
@@ -1,10 +1,13 @@
-﻿namespace SharedModel
+﻿using Starcounter;
+
+namespace SharedModel
 {
     public enum PetType
     {
         Dog, Cat, Horse, Rabbit, Fish
     }
 
+    [Database]
     public class Pet
     {
         public string Name { get; set; }
