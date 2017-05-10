152a0a6948760b0daba2eafcac85522e3e2ddd4e
Erlend <elandroe@icloud.com>
2017-05-10 09:37:13 +0200

    create SharedModels

---
+++ SharedModel/Entity1.cs
@@ -0,0 +1,11 @@
+﻿using Starcounter;
+
+namespace SharedModel
+{
+    [Database]
+    public class PetOwner
+    {
+        public string Name;
+        public Pet Pet;
+    }
+}
---
+++ SharedModel/Pet.cs
@@ -0,0 +1,16 @@
+﻿using Starcounter;
+
+namespace SharedModel
+{
+    public enum PetType
+    {
+        Cat, Dog, Fish, Bird, Horse
+    }
+
+    [Database]
+    public class Pet
+    {
+        public string Name { get; set; }
+        public PetType Type { get; set; }
+    }
+}
