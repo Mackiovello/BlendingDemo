8c3fa92c6466b07294b403f1032cdcea5af1a8d8
Erlend <elandroe@icloud.com>
2017-05-10 12:51:23 +0200

    complete renaming

--- src/PetApp/Properties/AssemblyInfo.cs
+++ src/PetApp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Starcounter.Internal;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PetApp")]
+[assembly: AssemblyTitle("PetImageProvider")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PetApp")]
+[assembly: AssemblyProduct("PetImageProvider")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
